1f70b29c51335c06c7520c690c08e6d4f6d26560
Sean McLaughlin <mclaughlin6464@gmail.com>
2018-05-07 16:07:50 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='uatu',
+        version='0.1',
+        description='Uatu: Probabilistic CNNs for cosmological inference.',
+        author='Sean McLaughlin',
+        author_email='swmclau2@stanford.edu',
+        url='https://github.com/mclaughlin6464/uatu',
+        packages=['uatu', 'uatu.simulations'])
