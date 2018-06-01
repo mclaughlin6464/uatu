8e06127bd44984d5531a444b3b2992b342878da2
Sean McLaughlin <mclaughlin6464@gmail.com>
2018-06-01 09:08:04 -0700

    Added some batch scripts.

---
+++ bin/make_cov_mat.py
@@ -0,0 +1,15 @@
+import nbodykit
+from uatu.watchers.traditional import *
+import numpy as np
+from time import time
+
+t0 = time()
+fname = '/home/users/swmclau2/scratch/UatuTraining3/'
+
+bins = np.logspace(-0.5, 1.6, 10)
+np.savetxt('/home/users/swmclau2/scratch/uatu_bins.npy', bins)
+
+cov = compute_cov_from_all_boxes(bins, fname)
+
+print time() - t0
+np.savetxt('/home/users/swmclau2/scratch/uatu_cov.npy', cov)
