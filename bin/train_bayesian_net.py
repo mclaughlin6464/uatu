cccaf394d5f150321ad8163690e7a8f7fd3e9d5d
mclaughlin6464 <mclaughlin6464@gmail.com>
2018-06-08 09:24:48 -0700

    merging.

--- bin/train_bayesian_net.py
+++ bin/train_bayesian_net.py
@@ -7,19 +7,19 @@ t0 = time()
 #dir = '/home/users/swmclau2/scratch/UatuTraining3/'
 dir = '../data/'
 
-X, y = get_all_xy(dir, max = 5)
+X, y = get_all_xy(dir, max = 10)
 #X, y = get_xy_from_dir( path.join(dir, 'Box000'), 0)
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.7)
 
 #X_val,X_test, y_val, t_test = train_test_split(X_test, y_test, train_size = 0.6)
 
-train_dset = Dataset(X_train,y_train, 1, shuffle=True, augment=True)
+train_dset = Dataset(X_train,y_train, 32, shuffle=True, augment=True)
 #val_dset = Dataset(X_val,y_val, 30, shuffle=True, augment=True)
-test_dset = Dataset(X_test, y_test, 1, shuffle=True, augment=True)
+test_dset = Dataset(X_test, y_test, 32, shuffle=True, augment=True)
 
 data = (train_dset, test_dset, None)
 
 device = "/device:GPU:0"
 #device = '/cpu:0'
 print time() - t0
-train(shallow_bayesian_convnet_init_fn, standard_optimizer_init_fn, bayes_cost_fn, data, device, num_epochs = 1, fname = './tmp_bayes', print_every = 1, lr = 0.0005) 
+train(shallow_bayesian_convnet_init_fn, standard_optimizer_init_fn, bayes_cost_fn, data, device, num_epochs = 500, fname = './tmp_bayes', print_every = 1, lr = 0.001) 
