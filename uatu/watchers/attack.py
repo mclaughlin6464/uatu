c22e7db17c91f7d659a0e6c973e42f6dec87dadd
Sean McLaughlin <mclaughlin6464@gmail.com>
2019-03-03 17:07:11 -0800

    Wrote a function to perform adversarial attacks.

--- uatu/watchers/attack.py
+++ uatu/watchers/attack.py
@@ -1,5 +1,5 @@
 """
-Make saliency maps
+Perform adversarial attacks on images
 """
 try:
     import tensorflow as tf
@@ -7,35 +7,35 @@ except:
     pass
 import numpy as np
 
-def compute_saliency_maps(model_init_fn, cost_fn, device, fname, data):
+def compute_attacked_maps(model_init_fn, cost_fn, fname, data, target_y_np):
 
     tf.reset_default_graph()
-    with tf.device(device):
-        x = tf.placeholder(tf.float32, [None, 64,64,64,1])
-        y = tf.placeholder(tf.float32, [None,2])
+    x = tf.placeholder(tf.float32, [None, 256, 256,1])
+    y = tf.placeholder(tf.float32, [None,2])
 
-        training = tf.placeholder(tf.bool, name='training')
+    training = tf.placeholder(tf.bool, name='training')
+    preds = model_init_fn(x, training)
 
-        preds = model_init_fn(x, training)
+    loss = cost_fn(y, preds)
+    grads = tf.gradients(loss, x)
 
-        #correct_scores = tf.gather_nd(model.scores,
-        #                              tf.stack((tf.range(X.shape[0]), model.labels), axis=1))
-
-        loss = cost_fn(y, preds)
-        grads = tf.gradients(loss, x)
+    # learning rate maybe needed?
+    dX = tf.divide(grads, tf.norm(grads))
 
     with tf.device('/cpu:0'):
         saver = tf.train.Saver()
 
     with tf.Session() as sess:
         saver.restore(sess, fname)
-        saliencies = []
-        for i, (x_np,  y_np) in enumerate(data):
-            feed_dict = {x: x_np,y:y_np, training: False}
-            #loss_np, update_ops_np = sess.run([loss,update_ops], feed_dict=feed_dict)
-            grads_np = sess.run([grads], feed_dict=feed_dict)[0][0]
-            saliency = np.max(np.abs(grads_np), axis=-1)
+        attacked_maps = []
+        for i, (x_np,  _) in enumerate(data):
+            x_attacked_np = x_np.copy()
+            for i in xrange(100):
+                feed_dict = {x: x_attacked_np,y:target_y_np, training: False}
+                #loss_np, update_ops_np = sess.run([loss,update_ops], feed_dict=feed_dict)
+                dX_np = sess.run([dX], feed_dict=feed_dict)
+                x_attacked_np+=dX_np
 
-            saliencies.append(saliency)
+            attacked_maps.append(x_attacked_np)
 
-        return np.array(saliencies)
+        return np.array(attacked_maps)
