83e8b67df2f8d35de429e0f289150c594f3b02f1
Sean McLaughlin <mclaughlin6464@gmail.com>
2019-10-01 16:38:03 -0700

    added adversarial training via a cursed decorator, gonna have to test though.

---
+++ uatu/watchers/adversarial_train.py
@@ -0,0 +1,60 @@
+"""
+Perform adversarial training against NNs
+"""
+import tensorflow as tf
+from functools import wraps
+
+from .train import standard_abs_cost_fn, standard_cost_fn, bayes_cost_fn, original_bayes_cost_fn
+
+def adversarial(epsilon = 0.01, K=10):
+    """
+    Decorator to convert loss functions into adversarial training loss functions
+    :param epsilon:
+        Radius of the norm-ball within which the adversarial examples live. Default 0.01
+    :param K:
+        Number of gradient steps to perform in the construction of adversarial examples. Default 10.
+    :return:
+        func, decorated function
+    """
+    def actual_decorator(loss_func):
+        @wraps(loss_func)
+        def _func(y, preds, x, model):
+
+            loss = loss_func(y, preds)
+            grads = tf.gradients(loss, x)
+
+            dx = tf.clip_by_value(tf.sign(grads), -epsilon, epsilon)
+
+            x+=dx
+            adv_preds = model(x, training=False)
+
+            for i in xrange(K-1):
+                loss = loss_func(y, adv_preds)
+                grads = tf.gradients(loss, x)
+
+                dx = tf.clip_by_value(tf.sign(grads), -epsilon, epsilon)
+
+                x+=dx
+                adv_preds = model(x, training=False)
+
+            return 0.5*loss + 0.5*loss_func(y, adv_preds)
+
+        return _func
+
+    return actual_decorator
+
+@adversarial()
+def adversarial_standard_cost_fn(y,preds):
+    return standard_cost_fn(y, preds)
+
+@adversarial()
+def adversarial_standard_abs_cost_fn(y,preds):
+    return standard_abs_cost_fn(y, preds)
+
+@adversarial()
+def adversarial_bayes_cost_fn(y,preds):
+    return bayes_cost_fn(y, preds)
+
+@adversarial()
+def adversarial_original_bayes_cost_fn(y, preds):
+    return original_bayes_cost_fn(y, preds)
