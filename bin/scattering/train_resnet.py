ad86da4e87aa1cc60cea66a21613ceab51670592
mclaughlin6464 <mclaughlin6464@gmail.com>
2019-12-19 06:43:33 -0800

    syncing

--- bin/scattering/train_resnet.py
+++ bin/scattering/train_resnet.py
@@ -25,8 +25,7 @@ t0 = time()
 dir = '/home/sean/Git/uatu/data/'
 fname = path.join(dir, 'UatuLightconeTraining.hdf5')
 
-batch_size = 8  
-
+batch_size = 2 
 
 train_dset = DatasetFromFile(fname,batch_size, shuffle=True, augment=True, train_test_split = 0.3, whiten = True, cache_size = 10, transform=torch.Tensor)
 val_dset = train_dset.get_test_dset()
@@ -45,10 +44,10 @@ for epoch in range(epochs):
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     #lr*=0.2
 
-    train(model, device, train_dset, optimizer, epoch+1, scattering)
+    train(model, device, train_dset, optimizer, epoch+1, scattering, print_every=500)
     val_test(model, device, val_dset, scattering)
 
-    torch.save(model.state_dict(), save_path)
+    #torch.save(model.state_dict(), save_path)
 
     if epoch%1==0:
         torch.save(model.state_dict(), path.join(output_dir, 'resnet_max_mode_%d_J_%d_epoch_%02d.pth'%(mode, J, epoch)))
