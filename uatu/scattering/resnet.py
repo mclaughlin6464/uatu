8da4be55475be4864cbfa92faa1e66d4b0fb5e0d
Sean McLaughlin <mclaughlin6464@gmail.com>
2020-01-23 15:10:57 -0800

    Added a deeper resnet for a few tests.

--- uatu/scattering/resnet.py
+++ uatu/scattering/resnet.py
@@ -62,7 +62,6 @@ class Scattering2dResNet(nn.Module):
 
         #self.layer3 = self._make_layer(BasicBlock, 64 * k, n)
         self.avgpool = nn.AdaptiveAvgPool2d(2)
-# TODO likely wrong
         self.fc = nn.Linear(64 * k * 4, 2)
 
     def _make_layer(self, block, planes, blocks, stride=1):
@@ -95,3 +94,62 @@ class Scattering2dResNet(nn.Module):
         return x
 
 
+class DeepResnet(nn.Module):
+    def __init__(self,  init_downsample_factor=4, in_channels=1, n_subplanes=2, n_sublocks=4, depth = [16, 32, 64, 64, 64, 64, 64] ):
+        super(DeepResnet, self).__init__()
+        self.inplanes = 16 * n_subplanes
+        self.ichannels = 16 * n_subplanes
+        self.K = in_channels
+        self.input_size = 256# int(256/(2**J))
+        self.downsample_size = self.input_size/init_downsample_factor
+
+        self.init_conv = nn.Sequential(
+            nn.BatchNorm2d(in_channels, eps=1e-5, affine=False),
+            nn.Conv2d(in_channels, self.ichannels,
+                  kernel_size=3, stride=1, padding=1, bias=False),
+            #nn.BatchNorm2d(self.ichannels),
+            nn.ReLU(True),
+            nn.AdaptiveAvgPool2d(self.downsample_size)
+        )
+        self.layers = []
+        self.depth = depth-1 if type(depth) is int else len(depth)
+        self.n_filters = [16 for i in range(depth)] if type(depth) is int else depth
+
+        for i, nf in enumerate(self.n_filters):
+            self.layers.append(self._make_layer(BasicBlock, nf * n_subplanes, n_sublocks))
+            setattr(self, "layer_%d"%i, self.layers[-1])
+
+        #self.layer3 = self._make_layer(BasicBlock, 64 * k, n)
+        self.avgpool = nn.AdaptiveAvgPool2d(4)
+
+        #self.fc = nn.Linear(64 * n_subplanes * 4, 2)
+        self.fc = nn.Linear(64 * n_subplanes * 16, 2)
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes),
+            )
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        # TODO function of J
+        x = x.view(x.size(0), self.K, self.input_size, self.input_size)
+        x = self.init_conv(x)
+
+        for l in self.layers:
+            x = l(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
